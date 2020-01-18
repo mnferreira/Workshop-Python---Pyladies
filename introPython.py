e4948817372bae94de16770d051554a465fecd33
mnferreira <mnferreira8@gmail.com>
2020-01-18 16:21:34 -0300

    exercícios while e for

--- introPython.py
+++ introPython.py
@@ -1,11 +1,32 @@
-numero = 5
-numeroChute = int(input('Chute um número de 1 a 10: '))
+# jogo de adivinhar números:
 
-if numero == numeroChute:
-    print('Você acertou')
+# numero = 5
+# numeroChute = int(input('Chute um número de 1 a 10: '))
 
-elif numero < numeroChute:
-    print('O número é menor do que ' + str(numeroChute))
+# if numero == numeroChute:
+#     print('Você acertou')
 
-else:
-    print('O número é maior do que '+ str(numeroChute))
+# elif numero < numeroChute:
+#     print('O número é menor do que ' + str(numeroChute))
+
+# else:
+#     print('O número é maior do que '+ str(numeroChute))
+
+
+# Exercício while:
+# contador = 1
+# preco = 0
+
+# while contador <= 7:
+#     nums = float(input('Insira o valor do lanche: '))
+#     preco = preco + nums
+#     contador = contador + 1
+
+# print(preco)
+
+#Exercício for:
+linguagens = ['Java', 'JavaScript', 'PHP', 'C', 'Python']
+
+for linguagem in linguagens:
+    if linguagem.startswith('P'):
+        print(linguagem)
